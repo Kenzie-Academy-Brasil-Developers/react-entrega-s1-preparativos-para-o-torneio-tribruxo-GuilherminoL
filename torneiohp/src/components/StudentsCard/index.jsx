ccb9b24ca97305de27e94da08979766579164216
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-16 08:57:54 -0300

    Making the project responsive

--- torneiohp/src/components/StudentsCard/index.jsx
+++ torneiohp/src/components/StudentsCard/index.jsx
@@ -3,8 +3,10 @@ const StudentsCard = ({ student }) => {
   return (
     <>
       <h3 className="studentName">{student.name}</h3>
-      <img className="studentImage" alt={student.name} src={student.image} />
-      <p className="studentHouse">House: {student.house} </p>
+      <div className="studentContent">
+        <img className="studentImage" alt={student.name} src={student.image} />
+        <p className="studentHouse">House: {student.house} </p>
+      </div>
     </>
   );
 };
